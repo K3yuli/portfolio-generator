b3a16de778750318b632fa9c1daa89e91930b9de
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2021-12-28 11:41:49 -0500

    node code

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+// notice the lack of parentheses around 'profileDataArr' parameter?
+const printProfileData = profileDataArr => {
+    // this...
+    for (let i = 0; i < profileDataArr.length; i+=1) {
+        console.log(profileDataArr[i]);
+    }
+    console.log('=================');
+
+    // is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
