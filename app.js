6a4f862d054f8622dbf72bb0d82b922e336f1606
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2021-12-29 17:11:02 -0500

    questions

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
+const inquirer = require('inquirer');
+// const fs = require('fs');
 
-const fs = require('fs');
+// // link page-template to app.js
+// const generatePage = require('./src/page-template');
 
-// link page-template to app.js
-const generatePage = require('./src/page-template.js');
-
-// now we have our profileDataArgvs array, which holds the user command-line arguments
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-// extract arguments and store them into distinct variables
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error (err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw (err);
 
-    console.log('Portfolio Complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio Complete! Check out index.html to see the output!');
+// });
 
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
 
 // // notice the lack of parentheses around 'profileDataArr' parameter?
 // const printProfileData = profileDataArr => {
