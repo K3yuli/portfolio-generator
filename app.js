f1c23ef7a5055e1abf11675239c79a2c7e304e0c
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2021-12-30 18:53:00 -0500

    questions

--- app.js
+++ app.js
@@ -13,15 +13,143 @@ const inquirer = require('inquirer');
 //     console.log('Portfolio Complete! Check out index.html to see the output!');
 // });
 
-inquirer
-.prompt([
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter GitHub Username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+// add another property to the prompt looking for information about the user called when.
+// This is like the validate method we used previously, but instead of passing the value entered for that specific question in as the parameter,
+// it passes an object of all of the answers given so far as an object.
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ]);
+};
+
+const promptProject = portfolioData => {
+    // if there's no 'projects' array property, create one
+    if(!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    console.log(`
+=================
+Add a New Project
+=================
+`);
+    return inquirer.prompt([
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is the name of your project? (Required)',
+        validate: projectNameInput => {
+            if (projectNameInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your project!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: projectDescriptionInput => {
+            if (projectDescriptionInput) {
+                return true;
+            } else { 
+                console.log('Please enter projects description!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: githubLinkInput => {
+            if(githubLinkInput) {
+                return true;
+            } else { console.log('Please enter GitHub link!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+    },
+    {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
     }
 ])
-.then(answers => console.log(answers));
+.then(projectData => {
+    portfolioData.projects.push(projectData);
+    if(projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+    } else {
+        return portfolioData;
+    }
+});
+};
+
+// Using Promises, we can chain the functions together using the then() method, as shown here:
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+});
 
 // // notice the lack of parentheses around 'profileDataArr' parameter?
 // const printProfileData = profileDataArr => {
