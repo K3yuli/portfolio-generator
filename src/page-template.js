998bbc6a32979de6d16456d779eac35a0575aeda
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2021-12-31 16:30:28 -0500

    html code

--- src/page-template.js
+++ src/page-template.js
@@ -1,22 +1,63 @@
 
-const generatePage = (name, github) => {
+// create the about section
+// This function, generateAbout(), will accept the about variable as a parameter, and if it doesn't exist, we'll simply return an empty string.
+// If it does exist, however, we'll return the entire <section> element.
+// Let's update the main function's template literal to execute generateAbout() where we want the content to go.
+const generateAbout = aboutText => {
+    if (!aboutText) {
+        return '';
+    }
+
     return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
+        <section class="my-3" id="about">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+        <p>${aboutText}</p>
+        </section>
+  `;
+}
+
+const generateProjects = projectsArr => {
+    return `
+      <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+          .filter(({ feature }) => feature)
+          .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 mb-2 bg-dark text-light p-3">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
+          })
+          .join('')}
   
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
+        ${projectsArr
+          .filter(({ feature }) => !feature)
+          .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
+          })
+          .join('')}
+        </div>
+      </section>
     `;
-};
+  };
 
 // in order for app.js to use info in page-template use...
 module.exports = generatePage;
