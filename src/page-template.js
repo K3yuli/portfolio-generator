23ac8471d7725b99c24c93bb1424445e95d68838
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-01 10:16:12 -0500

    finished

--- src/page-template.js
+++ src/page-template.js
@@ -1,22 +1,19 @@
-
 // create the about section
-// This function, generateAbout(), will accept the about variable as a parameter, and if it doesn't exist, we'll simply return an empty string.
-// If it does exist, however, we'll return the entire <section> element.
-// Let's update the main function's template literal to execute generateAbout() where we want the content to go.
 const generateAbout = aboutText => {
     if (!aboutText) {
-        return '';
+      return '';
     }
-
+  
     return `
-        <section class="my-3" id="about">
+      <section class="my-3" id="about">
         <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
         <p>${aboutText}</p>
-        </section>
-  `;
-}
-
-const generateProjects = projectsArr => {
+      </section>
+    `;
+  };
+  
+  // create the projects section
+  const generateProjects = projectsArr => {
     return `
       <section class="my-3" id="portfolio">
         <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
@@ -29,7 +26,7 @@ const generateProjects = projectsArr => {
               <h3 class="portfolio-item-title text-light">${name}</h3>
               <h5 class="portfolio-languages">
                 Built With:
-                ${languages.join(', ')}
+                ${languages.map(language => language).join(',')}
               </h5>
               <p>${description}</p>
               <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
@@ -58,6 +55,44 @@ const generateProjects = projectsArr => {
       </section>
     `;
   };
-
-// in order for app.js to use info in page-template use...
-module.exports = generatePage;
+  
+  // export function to generate entire page
+  module.exports = templateData => {
+    // destructure page data by section
+    const { projects, about, ...header } = templateData;
+  
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+      <header>
+        <div class="container flex-row justify-space-between align-center py-3">
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+          <nav class="flex-row">
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
+              header.github
+            }">GitHub</a>
+          </nav>
+        </div>
+      </header>
+      <main class="container my-5">
+        ${generateAbout(about)}
+        ${generateProjects(projects)}
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      </footer>
+    </body>
+    </html>
+    `;
+  };
